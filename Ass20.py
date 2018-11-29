947e84781d6cd9b8d3fe7eab19e8872d05d5a0f0
ArpitKubadia <arpitkubadia23@gmail.com>
2018-11-29 14:17:04 +0530

    Till 24

--- Ass20.py
+++
@@ -1,6 +0,0 @@
-n=int(input("Enter n: "))
-l=[0,1]
-for i in range(2,n):
-    l.append(l[i-1]+l[i-2])
-print(l)
-
--- Ass22.py
+++
@@ -1,6 +0,0 @@
-courses = ("Python Programming", "RDBMS", "Web Technology", "Software Engg.")
-electives = ("Business Intelligence", "Big Data Analytics")
-
-print(len(courses))
-print(courses)
-print(courses + electives)
